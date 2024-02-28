7cf052b1f3f1ea4b79f13c45b33ccc9c64195ea0
KAUA_MATHEUS <kauamatheussilva06@gmail.com>
2024-02-28 11:41:06 -0300

    Melhorei o brute_force.py com o intuito de poder utiliza-lo como aprendizado. Aprendi sobre o intertools, ferramenta muito legal para algumas operações específicas, utilizarei principalmente a função product(), para gerar possibilidades de combinações das senhas.

--- Projetos_python/brute_force.py
+++ Projetos_python/brute_force.py
@@ -1,8 +1,36 @@
-senha = '12345'
+import string, random
 
-def testador(senha, dig=len(senha)):
+alfabet = string.printable[:95]
+charlength = 3
 
-    return (dig * '1' + '2')
+def pass_generator(tamanho, alfabeto = alfabet):
+    senha = ''
+    while len(senha) < tamanho:
+        senha += random.choice(alfabeto)
+    return senha
+
+def testador_v1(senha = '123441'):
+    quant_digMn = len(str(senha))
+    val = '0' * quant_digMn
+    while True:
+        for c in range(quant_digMn):
+            val = str(int(val) + 1)
+            print(val)
+            if val == str(senha):
+                return print(val)
+            elif len((val)) > quant_digMn:
+                return None
+            else:
+                continue
+
+# def testador_v2(senha, alfabeto=alfabet):
 
 
-print(testador(senha))
+"""random_pass = pass_generator(charlength)
+print(random_pass)"""
+
+"""valores = 'abcd'
+lista = [x for x in valores]
+print(lista)"""
+
+
